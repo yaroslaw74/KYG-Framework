a6f677bd0a2cb01c2cd518724f5c26a34f9f20b8
yaroslaw74 <yaroslaw74@gmail.com>
2024-02-11 15:40:41 +0200

    Update

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+/**********************************************************************************
+ * @Project    KYG Framework
+ * @Version    1.0
+ * @Data       11.02.2024
+ *
+ * @Copyright  (C) 2024 Kataev Yaroslav Georgievich 
+ * @E-mail     yaroslaw74@gmail.com
+ * @License    GNU General Public License version 3 or later; see LICENSE.md
+ *********************************************************************************/
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Filesystem\Filesystem;
+class HomeController extends AbstractController
+{
+    private $Interface = [
+            'Theme' => 'start'
+        ];
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        $filesystem = new Filesystem();
+        return $this->render('home/index.html.twig', [
+            'Interface' => $this->Interface
+        ]);
+    }
+}
