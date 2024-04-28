27459c01c63c039081c5abe955b9a95dab9121b0
Ярослав <yaroslaw74@gmail.com>
2024-04-28 18:56:29 +0300

    Update bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,12 @@
 <?php
-
+/**********************************************************************************
+ * @Project    KYG Framework for business
+ * @Version    1.0,0
+ *
+ * @Copyright  (C) 2025 Kataev Yaroslav Georgievich 
+ * @E-mail     yaroslaw74@gmail.com
+ * @License    GNU General Public License version 3 or later; see LICENSE.md
+ *********************************************************************************/
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
