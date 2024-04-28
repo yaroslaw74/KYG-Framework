d9f7daf49dca8fd9a56cca8930f4030fa50335b7
Ярослав <yaroslaw74@gmail.com>
2024-04-28 19:52:53 +0300

    update

---
+++ src/EventSubscriber/UserLocaleSubscriber.php
@@ -0,0 +1,39 @@
+<?php
+
+/**********************************************************************************
+ * @Project    KYG Framework for business
+ * @Version    1.0, 0
+ *
+ * @Copyright  (C) 2025 Kataev Yaroslav Georgievich
+ * @E-mail     yaroslaw74@gmail.com
+ * @License    GNU General Public License version 3 or later;
+see LICENSE.md
+ *********************************************************************************/
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
+
+/**
+ * Stores the locale of the user in the session after the
+ * login. This can be used by the LocaleSubscriber afterwards.
+ */
+
+class UserLocaleSubscriber implements EventSubscriberInterface
+{
+    public function __construct(private RequestStack $requestStack) {}
+
+    public function onLoginSuccess(LoginSuccessEvent $event): void
+    {
+        $user = $event->getUser();
+
+        if (null !== $user->getLocale()) $this->requestStack->getSession()->set('_locale', $user->getLocale());
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [LoginSuccessEvent::class => 'onLoginSuccess'];
+    }
+}
