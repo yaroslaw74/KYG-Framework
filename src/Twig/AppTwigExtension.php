c735b1e53d53b4da74809d0b5fed71b4390358f1
Ярослав <yaroslaw74@gmail.com>
2024-07-01 11:28:31 +0300

    Create AppTwigExtension.php

---
+++ src/Twig/AppTwigExtension.php
@@ -0,0 +1,32 @@
+<?php
+/********************************************************************************
+ * @Project    KYG Framework for business
+ * @Version    1.0.0
+ *
+ * @Copyright  (C) 2025 Kataev Yaroslav Georgievich 
+ * @E-mail     yaroslaw74@gmail.com
+ * @License    GNU General Public License version 3 or later; see LICENSE.md
+ ********************************************************************************/
+namespace App\Twig;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+use Symfony\Component\Yaml\Yaml;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class AppTwigExtension extends AbstractExtension
+{
+    public function __construct(private ContainerBagInterface $params) {}
+    public function LocaleDirExtension(string $locale): string
+    {
+        $languages = Yaml::parseFile($this->params->get('app.modules_dir') . '/languages.yaml');
+        return $languages[$locale]['dir'];
+    }
+    
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('localedir', [$this, 'LocaleDirExtension'])
+        ];
+    }
+}
