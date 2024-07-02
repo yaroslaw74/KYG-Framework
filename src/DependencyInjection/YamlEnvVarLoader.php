988513d61a19983472f972b06398837cf5efe4a7
Ярослав <yaroslaw74@gmail.com>
2024-07-02 15:23:26 +0300

    Create YamlEnvVarLoader.php

---
+++ src/DependencyInjection/YamlEnvVarLoader.php
@@ -0,0 +1,33 @@
+<?php
+/**********************************************************************************
+* @Project    KYG Framework for business
+* @Version    1.0.0
+*
+* @Copyright  (C) 2025 Kataev Yaroslav Georgievich
+* @E-mail     yaroslaw74@gmail.com
+* @License    GNU General Public License version 3 or later;
+see LICENSE.md
+*********************************************************************************/
+namespace App\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\EnvVarLoaderInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Yaml\Yaml;
+
+final class YamlEnvVarLoader implements EnvVarLoaderInterface
+ {
+    public function __construct(private ContainerBagInterface $params) {}
+
+    public function loadEnvVars(): array
+ {
+        $fileName = $this->params->get('app.var_dir') . '/env.yaml';
+        $filesystem = new Filesystem();
+        if (!$filesystem->exists($fileName)) {
+            return [];
+        } else {
+            $content = Yaml::parseFile( $fileName );
+            return $content['vars'];
+        }
+    }
+}
