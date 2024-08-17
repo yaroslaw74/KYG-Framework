a2e118611675a1d1a2d2f8c54f78da500317410d
Ярослав <yaroslaw74@gmail.com>
2024-08-17 17:54:49 +0300

    update

--- modules/Users/Form/RegistrationFormType.php
+++ modules/Users/Form/RegistrationFormType.php
@@ -13,38 +13,49 @@ use App\Modules\Users\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationFormType extends AbstractType
 {
+    public function __construct(private TranslatorInterface $translator) {}
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
+            ->add('username', TextType::class, [
+                'label' => $this->translator->trans('form.username', [], 'registration')
+            ])
             ->add('agreeTerms', CheckboxType::class, [
-                                'mapped' => false,
+                'label' => $this->translator->trans('form.terms', [], 'registration'),
+                'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => $this->translator->trans('message.terms', [], 'registration'),
                     ]),
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
-                                // instead of being set onto the object directly,
+                // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'label' => $this->translator->trans('form.password', [], 'registration'),
+                'toggle' => true,
+                'hidden_label' => 'login.hide',
+                'visible_label' => 'login.show',
+                'toggle_translation_domain' => 'login',
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => $this->translator->trans('message.password', [], 'registration'),
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => $this->translator->trans('message.min', [], 'registration'),
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
