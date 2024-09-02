9c7258495d97f4654b18a8e4033929847349eea0
Ярослав <yaroslaw74@gmail.com>
2024-09-02 15:39:42 +0300

    update

---
+++ modules/Users/Form/ResetPasswordRequestFormType.php
@@ -0,0 +1,40 @@
+<?php
+/**********************************************************************************
+ * @Project    KYG Framework for business
+ * @Version    1.0.0
+ *
+ * @Copyright  (C) 2025 Kataev Yaroslav Georgievich
+ * @E-mail     yaroslaw74@gmail.com
+ * @License    GNU General Public License version 3 or later, see LICENSE.md
+ *********************************************************************************/
+namespace App\Modules\Users\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class ResetPasswordRequestFormType extends AbstractType
+{
+    public function __construct(private TranslatorInterface $translator) {}
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'attr' => ['autocomplete' => 'email'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => $this->translator->trans('message.email', [], 'reset_password'),
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
