25f464b8c4bdb7627340e905988f780adabc00de
Ярослав <yaroslaw74@gmail.com>
2024-09-03 20:48:13 +0300

    update

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Shivas\VersioningBundle\ShivasVersioningBundle::class => ['all' => true],
 ];
