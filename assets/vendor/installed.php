d8c557d4a8da9e72a0ea768b81f05c9bb5113a62
yaroslaw74 <yaroslaw74@gmail.com>
2024-09-17 17:35:01 +0300

    update

---
+++ assets/vendor/installed.php
@@ -0,0 +1,22 @@
+<?php return array (
+  '@hotwired/stimulus' => 
+  array (
+    'version' => '3.2.2',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@hotwired/turbo' => 
+  array (
+    'version' => '8.0.6',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+);
