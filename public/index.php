df49fa7f2e2a231641f289975ee82b10d1af5ab0
yaroslaw74 <yaroslaw74@gmail.com>
2024-09-18 19:11:23 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,20 +1,8 @@
 <?php
-/**********************************************************************************
-* @Project    KYG Framework for business
-* @Version    1.0.0
-*
-* @Copyright  (C) 2026 Kataev Yaroslav Georgievich
-* @E-mail     yaroslaw74@gmail.com
-* @License    GNU General Public License version 3 or later, see LICENSE.md
-*********************************************************************************/
+
 use App\Kernel;
-use App\Modules\Install\Resources\Start;
 
-$dir = dirname(__DIR__);
-require_once($dir . '/vendor/autoload_runtime.php');
-
-$start = new Start($dir);
-$start->StartApp();
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
