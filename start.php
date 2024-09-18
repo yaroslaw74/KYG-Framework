41b561de8d8c0b4880b990e8447bf790c8bbdfa2
yaroslaw74 <yaroslaw74@gmail.com>
2024-09-18 20:13:50 +0300

    Update start.php

--- start.php
+++ start.php
@@ -12,15 +12,15 @@ if (!file_exists($dir . DIRECTORY_SEPARATOR . 'migrations'))
     mkdir($dir . DIRECTORY_SEPARATOR . 'migrations');
 $public = $dir . DIRECTORY_SEPARATOR . 'public';
 if (!file_exists($public . DIRECTORY_SEPARATOR . 'additions'))
-    mkdir($public . DIRECTORY_SEPARATOR . 'additions', 0777);
+    mkdir($public . DIRECTORY_SEPARATOR . 'additions');
 if (!file_exists($public . DIRECTORY_SEPARATOR . 'media'))
-    mkdir($public . DIRECTORY_SEPARATOR . 'media', 0777);
+    mkdir($public . DIRECTORY_SEPARATOR . 'media');
 if (!file_exists($public . DIRECTORY_SEPARATOR . 'translations'))
-    mkdir($public . DIRECTORY_SEPARATOR . 'translations', 0777);
+    mkdir($public . DIRECTORY_SEPARATOR . 'translations');
 $config = $public . DIRECTORY_SEPARATOR . 'config';
 $install = $dir . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR . 'Install' . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'config';
 if (!file_exists($config)) {
-    mkdir($config, 0777);
+    mkdir($config);
     $file = array_diff(scandir($install), ['..', '.']);
     foreach($file as $name)
     {
