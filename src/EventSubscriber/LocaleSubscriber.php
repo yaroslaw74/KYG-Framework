72155dc8396cff7e482da789cbc275cd4c2a5983
yaroslaw74 <yaroslaw74@gmail.com>
2024-10-15 21:40:14 +0300

    update

---
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,31 @@
+<?php
+/**********************************************************************************
+ * @Project    KYG Framework for business
+ * @Version    1.0.0
+ *
+ * @Copyright  (C) 2026 Kataev Yaroslav Georgievich
+ * @E-mail     yaroslaw74@gmail.com
+ * @License    GNU General Public License version 3 or later, see LICENSE.md
+ *********************************************************************************/
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+        $locale = $request->request->getString('_locale');
+        if ($locale != '')
+        {
+            $request->setLocale($locale);
+        }
+    }
+    public static function getSubscribedEvents(): array
+    {
+        return [KernelEvents::REQUEST => [['onKernelRequest', 20]]];
+    }
+}
