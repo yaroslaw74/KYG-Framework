d83d98c51f64a36fa8840ea6b3238f1d42b11a1c
yaroslaw74 <yaroslaw74@gmail.com>
2024-10-15 19:02:11 +0300

    update

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
+    Symfony\UX\Dropzone\DropzoneBundle::class => ['all' => true],
 ];
