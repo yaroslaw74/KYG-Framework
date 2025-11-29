c4917bf99a9c9d961ce6e3afa1683afed0bd2036
yaroslaw74 <yaroslaw74@gmail.com>
2025-11-29 19:49:44 +0200

    Update reference.php

--- config/reference.php
+++ config/reference.php
@@ -1489,6 +1489,12 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
  *         server?: scalar|null, // Default: "http://www.plantuml.com/plantuml"
  *     },
  * }
+ * @psalm-type DamaDoctrineTestConfig = array{
+ *     enable_static_connection?: mixed, // Default: true
+ *     enable_static_meta_data_cache?: bool, // Default: true
+ *     enable_static_query_cache?: bool, // Default: true
+ *     connection_keys?: list<mixed>,
+ * }
  * @psalm-type ConfigType = array{
  *     imports?: ImportsConfig,
  *     parameters?: ParametersConfig,
@@ -1538,6 +1544,7 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
  *         security?: SecurityConfig,
  *         monolog?: MonologConfig,
  *         doctrine_diagram?: DoctrineDiagramConfig,
+ *         dama_doctrine_test?: DamaDoctrineTestConfig,
  *     },
  *     ...<string, ExtensionType|array{ // extra keys must follow the when@%env% pattern or match an extension alias
  *         imports?: ImportsConfig,
