321f5cf23804e7b418bf2a3c41f439c6aff6c8ed
yaroslaw74 <yaroslaw74@gmail.com>
2025-11-29 20:47:21 +0200

    Create icons-fonts.js

---
+++ assets/icons-fonts.js
@@ -0,0 +1,81 @@
+/*
+ * Bootstrap Icons
+ */
+import "./vendor/bootstrap-icons/font/bootstrap-icons.min.css";
+
+/*
+ * Boxicons
+ */
+import "./vendor/boxicons/css/boxicons.min.css";
+
+/*
+ * Feather icons
+ */
+import "./icon-fonts/feather/feather.css";
+
+/*
+ * Fontawesome Icons
+ */
+import "./vendor/@fortawesome/fontawesome-free/css/all.min.css";
+
+/*
+ * Ionicons
+ */
+import "./vendor/ionicons/dist/css/ionicons.min.css";
+
+/*
+ * Line Awesome Icons
+ */
+import "./vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css";
+
+/*
+ * Material-icons
+ */
+import "./vendor/material-icons/iconfont/material-icons.css";
+
+/*
+ * Material-symbols
+ */
+import "./vendor/material-symbols/index.css";
+
+/*
+ * Materialdesign Icons
+ */
+import "./vendor/@mdi/font/css/materialdesignicons.min.css";
+
+/*
+ * Pe7 Icons
+ */
+import "./icon-fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css";
+import "./icon-fonts/pe-icon-7-stroke/css/helper.css";
+
+/*
+ * Remix Icons
+ */
+import "./vendor/remixicon/fonts/remixicon.css";
+
+/*
+ * Simpleline Icons
+ */
+import "./vendor/simple-line-icons/css/simple-line-icons.css";
+
+/*
+ * Tabler Icons
+ */
+import "./vendor/@tabler/icons-webfont/dist/tabler-icons.min.css";
+
+/*
+ * Themify Icons
+ */
+import "./vendor/cd-themify-icons/index.css";
+
+/*
+ * Typicon Icons
+ */
+import "./vendor/typicons.font/src/font/typicons.css";
+
+/*
+ * Weather Icons
+ */
+import "./icon-fonts/weather-icons-master/css/weather-icons.min.css";
+import "./icon-fonts/weather-icons-master/css/weather-icons-wind.min.css";
