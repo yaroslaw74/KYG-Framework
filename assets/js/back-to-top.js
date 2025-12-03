83f581ab84972475d8e3f41767ce3e10f9a423b2
yaroslaw74 <yaroslaw74@gmail.com>
2025-12-03 09:41:54 +0200

    Create back-to-top.js

---
+++ assets/js/back-to-top.js
@@ -0,0 +1,17 @@
+(()=>{
+    const scrollToTop = document.querySelector(".scrollToTop");
+    const $rootElement = document.documentElement;
+    const $body = document.body;
+    window.onscroll = () => {
+        const scrollTop = window.scrollY || window.pageYOffset;
+        const clientHt = $rootElement.scrollHeight - $rootElement.clientHeight;
+        if (window.scrollY > 100) {
+            scrollToTop.style.display = "flex";
+        } else {
+            scrollToTop.style.display = "none";
+        }
+    };
+    scrollToTop.onclick = () => {
+        window.scrollTo(0, 0);
+    };
+})();
