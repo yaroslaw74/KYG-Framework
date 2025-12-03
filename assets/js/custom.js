cc223bce036dd0c09746cbf9aef33a235e9b2b37
yaroslaw74 <yaroslaw74@gmail.com>
2025-12-03 09:41:58 +0200

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -298,24 +298,6 @@
     }, 10);
     /* count-up */
 
-    /* back to top */
-    const scrollToTop = document.querySelector(".scrollToTop");
-    const $rootElement = document.documentElement;
-    const $body = document.body;
-    window.onscroll = () => {
-        const scrollTop = window.scrollY || window.pageYOffset;
-        const clientHt = $rootElement.scrollHeight - $rootElement.clientHeight;
-        if (window.scrollY > 100) {
-            scrollToTop.style.display = "flex";
-        } else {
-            scrollToTop.style.display = "none";
-        }
-    };
-    scrollToTop.onclick = () => {
-        window.scrollTo(0, 0);
-    };
-    /* back to top */
-
     /* header dropdowns scroll */
     const myHeadernotification = document.getElementById("header-notification-scroll");
     new SimpleBar(myHeadernotification, { autoHide: true });
